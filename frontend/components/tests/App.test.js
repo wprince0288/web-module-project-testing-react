3ede1a8c82c2bebc1887da17311a9cade2bfabd8
Walter Prince <wprince0288@gmail.com>
2024-11-27 20:57:00 -0600

    App.test finished

--- frontend/components/tests/App.test.js
+++ frontend/components/tests/App.test.js
@@ -17,27 +17,36 @@ describe('Stranger Things App', () => {
   })
   test('App mounts without crashing', () => {
     // 👉 TASK: print the simulated DOM using screen.debug
+    screen.debug()
   })
   test('App renders the correct texts', async () => {
     // 👉 TASK: click on the button that displays "Press to Get Show Data"
-
+    await user.click(screen.getByText('Press to Get Show Data'))
     // 👉 TASK: create a waitFor and await for the following to be true:
     //    - The text "Press to Get Show Data" is no longer in the DOM
     //    - The text "Stranger Things" exists in the DOM
     //    - The text "A love letter to the '80s classics that captivated a generation" exists in the DOM
     //    - The text "Select A Season" exists in the DOM
     // ❗ You will need { exact: false } to select the longer text
-
+    await waitFor(() => {
+      expect(screen.queryByText('Press to Get Show Data'))
+        .not.toBeInTheDocument()
+      screen.getByText('Stranger Things')
+      screen.getByText("A love letter to the '80s classics that captivated a generation", { exact: false })
+      screen.getByText("Select A Season")
+    })
     // 👉 TASK: select Season 2 from the dropdown
     // ❗ Don't forget user actions need the await keyword
     // ❗ Use the selectOptions user action
     // ❗ Grab the select element using querySelector
-
+    await user.selectOptions(document.querySelector('select'), '1')
     // 👉 TASK: create the following assertions:
     //    - The text "Season 2, Episode 1" exists in the DOM
     //    - The text "Chapter One: MADMAX" exists in the DOM
     //    - The text "One year after the events with the Upside Down and the Demogorgon" exists in the DOM
     // ❗ You will need { exact: false } to select the longer text
-
+    screen.getByText("Season 2, Episode 1")
+    screen.getByText("Chapter One: MADMAX")
+    screen.getByText("One year after the events with the Upside Down and the Demogorgon", { exact: false })
   })
 })
